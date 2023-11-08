5ed1770f3282e37af9b68c6e3fe106b531d83a8c
Kevin <kevinsilvarj10@gmail.com>
2023-11-08 20:17:34 -0300

    Retornando o usuário e o token com base na autenticação

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { FiMail, FiLock } from "react-icons/fi"
-import { Container, Form, Background } from "./styles";
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
 
+import { useAuth } from "../../hooks/auth";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Background } from "./styles";
+
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { SignIn } = useAuth();
+
+  function handleSignIn() {
+    SignIn({ email, password });
+  }
 
   return (
     <Container>
@@ -14,23 +27,25 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input 
+        <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
-        
-        <Input 
+
+        <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
-      
+
       <Background />
     </Container>
   );
